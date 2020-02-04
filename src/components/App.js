765b7ccba93b3e2efc95bd60043c80aa867c5716
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-02-04 09:59:05 +0530

    added card view for posts in front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import logo from '../logo.png';
+import Identicon from 'identicon.js';
 import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json';
 import Navbar from './Navbar';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      accounts: ''
+      accounts: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: []
     }
   }
   async componentWillMount() {
@@ -32,9 +36,27 @@ class App extends Component {
     const web3 = window.web3
     // load accounts
     const accounts = await web3.eth.getAccounts()
-    this.setState({
-      accounts: accounts[0]
-    })
+    this.setState({ accounts: accounts[0] })
+    // Network // ID
+    const networkId = await web3.eth.net.getId()
+    const networkData = SocialNetwork.networks[networkId]
+
+    if(networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      // Load posts
+      for(var i = 1; i <= postCount; i++){
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({ posts:[...this.state.posts, post]})
+      }
+      console.log({posts: this.state.posts});
+    }else {
+      window.alert('SocialNetwork contract not deployed to detected network')
+    }
+    // Address
+    // ABI
   }
   render() {
     return (
@@ -42,33 +64,42 @@ class App extends Component {
         <Navbar account={this.state.accounts} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Social Network Platform</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{maxWidth: '500px'}}>
+              <div className="  content mr-auto ml-auto">
+                { this.state.posts.map((post, key) => {
+                  return(
+                    <div className="card mb-4" key={key} >
+                      <div className="card-header">
+                        <img
+                          className='mr-2'
+                          width='30'
+                          height='30'
+                          src={`data:image/png;base64,${new Identicon(this.state.accounts, 30).toString()}`}
+                          />
+                        <small className="text-muted">{post.author}</small>
+                      </div>
+                      <ul id="postList" className="list-group list-group-flush">
+                        <li className="list-group-item">
+                          <p>{post.content}</p>
+                        </li>
+                        <li key={key} className="list-group-item py-2">
+                          <small className="float-left mt-1 text-muted">TIPS: {window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')} Tip</small>
+                          <button className="btn btn-link btn-sm float-right pt-0">
+                            <span>
+                              TIP 0.1 ETH
+                            </span>
+                          </button>
+                        </li>
+                      </ul>
+                    </div>
+                  )
+                })}
               </div>
             </main>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
