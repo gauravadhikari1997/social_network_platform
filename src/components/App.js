8cac7f667c9f6318940c94fc6f6bc04cdf93c3e2
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-04-26 15:50:42 +0530

    added rinkeby deployment

--- src/components/App.js
+++ src/components/App.js
@@ -1,93 +1,108 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import './App.css';
-import SocialNetwork from '../abis/SocialNetwork.json';
-import Navbar from './Navbar';
-import Main from './Main';
+import React, { Component } from "react";
+import Web3 from "web3";
+import "./App.css";
+import SocialNetwork from "../abis/SocialNetwork.json";
+import Navbar from "./Navbar";
+import Main from "./Main";
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      accounts: '',
+      accounts: "",
       socialNetwork: null,
       postCount: 0,
       posts: [],
       loading: true
-    }
+    };
     this.createPost = this.createPost.bind(this);
     this.tipPost = this.tipPost.bind(this);
   }
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
   async loadWeb3() {
     if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        "Non-Ethereum browser detected. You should consider trying MetaMask!"
+      );
     }
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
+    const web3 = window.web3;
     // load accounts
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ accounts: accounts[0] })
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ accounts: accounts[0] });
     // Network // ID
-    const networkId = await web3.eth.net.getId()
-    const networkData = SocialNetwork.networks[networkId]
+    const networkId = await web3.eth.net.getId();
+    const networkData = SocialNetwork.networks[networkId];
 
-    if(networkData) {
-      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
-      this.setState({ socialNetwork })
-      const postCount = await socialNetwork.methods.postCount().call()
-      this.setState({ postCount })
+    if (networkData) {
+      const socialNetwork = web3.eth.Contract(
+        SocialNetwork.abi,
+        networkData.address
+      );
+      this.setState({ socialNetwork });
+      const postCount = await socialNetwork.methods.postCount().call();
+      this.setState({ postCount });
       // Load posts
-      for(var i = 1; i <= postCount; i++){
-        const post = await socialNetwork.methods.posts(i).call()
-        this.setState({ posts:[...this.state.posts, post]})
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call();
+        this.setState({ posts: [...this.state.posts, post] });
       }
       // Sort posts to view highest tipped posts at top
       this.setState({
         posts: this.state.posts.sort((a, b) => b.tipAmount - a.tipAmount)
-      })
-      this.setState({ loading: false })
-    }else {
-      window.alert('SocialNetwork contract not deployed to detected network')
+      });
+      this.setState({ loading: false });
+    } else {
+      window.alert("SocialNetwork contract not deployed to detected network");
     }
     // Address
     // ABI
   }
   createPost(content) {
-    this.setState({ loading: true })
-    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.accounts}).once('receipt', (receipt) => {
-      this.setState({ loading: false})
-    })
+    this.setState({ loading: true });
+    this.state.socialNetwork.methods
+      .createPost(content)
+      .send({ from: this.state.accounts })
+      .then("receipt", receipt => {
+        window.location.reload(false);
+        this.setState({ loading: false });
+      });
   }
 
   tipPost(id, tipAmount) {
-    this.setState({ loading: true })
-    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.accounts, value: tipAmount}).once('receipt', (receipt) => {
-      this.setState({ loading: false })
-    })
+    this.setState({ loading: true });
+    this.state.socialNetwork.methods
+      .tipPost(id)
+      .send({ from: this.state.accounts, value: tipAmount })
+      .once("receipt", receipt => {
+        this.setState({ loading: false });
+      });
   }
   render() {
     return (
       <div>
         <Navbar account={this.state.accounts} />
-        { this.state.loading
-          ?
-          <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
-          :
-          <Main posts={this.state.posts} createPost={this.createPost} tipPost={this.tipPost} />
-        }
+        {this.state.loading ? (
+          <div id="loader" className="text-center mt-5">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <Main
+            posts={this.state.posts}
+            createPost={this.createPost}
+            tipPost={this.tipPost}
+          />
+        )}
       </div>
     );
   }
--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,29 @@
-require('babel-register');
-require('babel-polyfill');
+require("babel-register");
+require("babel-polyfill");
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic =
+  "wreck method very basket icon skate clarify donate staff forget address wise";
 
 module.exports = {
   networks: {
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/v3/1905eb9b9eff4d1a8d19d5c5c7163f85"
+        );
+      },
+      network_id: 4
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
-    },
+    }
   },
-  contracts_directory: './src/contracts/',
-  contracts_build_directory: './src/abis/',
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
   compilers: {
     solc: {
       optimizer: {
@@ -19,4 +32,4 @@ module.exports = {
       }
     }
   }
-}
+};
